6829833444d5c4abf3bb52f701137126fd324ead
jakub <jakub.webdesign@gmail.com>
2018-02-24 11:45:34 +0100

    add css and js

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,15 @@
 var imageCount = 1;
-var imgTotal = 5;
+var imgTotal = 3;
 
 function slideJs(img){
   var Image = document.getElementById("img");
+  var checker = 1;
   imageCount = imageCount + img;
+  if (imageCount > imgTotal) {
+    imageCount = 1;
+  }
+  if(imageCount < checker){
+    imageCount = imgTotal;
+  }
   Image.src = "img/img"+imageCount+ ".jpg";
 }
